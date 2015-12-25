4b48e28aa73c89dcd2e71266a871e25e79d5da17
Ashish Mishra <ashish@gooru.org>
2015-12-25 11:21:34 +0530

    Inclucde some dependencies and provide a main verticle

---
+++ src/main/java/org/gooru/nucleus/jobs/bootstrap/JobVerticle.java
@@ -0,0 +1,10 @@
+package org.gooru.nucleus.jobs.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class JobVerticle extends AbstractVerticle {
+
+}
